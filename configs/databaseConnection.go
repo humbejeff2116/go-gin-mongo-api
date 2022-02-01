430911b97c2bfbfd01dd49933125668a40972d3a
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-01 12:51:33 +0000

    fix get collection bug

--- configs/databaseConnection.go
+++ configs/databaseConnection.go
@@ -24,25 +24,18 @@ import (
 var MongodbClient *mongo.Client = ConnectToMongDb();
 
 
-
-
 func ConnectToMongDb() *mongo.Client{
 	serverConfigs := SetServerConfigurations();
-	contxt, cancel := context.WithTimeout(context.Background(), 10*time.Second);
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second);
 	defer cancel()
 	// Create a new client and connect to the server
-	client, err := mongo.Connect(contxt, options.Client().ApplyURI(serverConfigs.DatabaseURI));
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(serverConfigs.DatabaseURI));
 	if err != nil {
+		// log.Fatal(err)
 		panic(err)
 	}
-	defer func() {
-		err = client.Disconnect(contxt);
-		if  err != nil {
-			panic(err)
-		}
-	}()
 	// Ping the primary
-	err = client.Ping(contxt, readpref.Primary());
+	err = client.Ping(ctx, readpref.Primary());
 	if  err != nil {
 		panic(err)
 	}
@@ -55,4 +48,3 @@ func GetCollection(client *mongo.Client, databaseName, collectionName string) *m
     collection := client.Database(databaseName).Collection(collectionName);
     return collection;
 }
-
