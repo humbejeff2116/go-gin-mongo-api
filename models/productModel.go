92b20b94178a3e350e73a2815bd06ee8da9c0eeb
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-02 20:33:42 +0000

    modify product model struct properties

--- models/productModel.go
+++ models/productModel.go
@@ -11,23 +11,20 @@ import (
 
 type ProductModel struct {
 	Id primitive.ObjectID `json:"id,omitempty"`
-	UserName string `json:"userName,omitempty" validate:"required"`
-    UserId string `json:"userId,omitempty" validate:"required"`
-    UserProfileImage string  `json:"userProfileImage,omitempty"`
-    UserEmail string `json:"userEmail,omitempty" validate:"required"`
     ProductName string `json:"productName,omitempty" validate:"required"`
     ProductCategory string `json:"productCategory,omitempty" validate:"required"`
-    ProductCountry string `json:"productCountry,omitempty" validate:"required"`
-    ProductState string `json:"productState,omitempty" validate:"required"`
-    ProductUsage string `json:"productUsage,omitempty"`
-    ProductCurrency string `json:"productCurrency,omitempty" validate:"required"`
     ProductPrice string `json:"productPrice,omitempty" validate:"required"`
-    ProductContactNumber string `json:"productContactNumber,omitempty" validate:"required"`
-    // QuantitySold [{}]
-    // productImages: [{}],
-    // stars: [{}],
-    // unstars: [{}],
-    // comments: [{}],
-    // interests: [{}],
-    // createdAt: { type: Date, default: Date.now }
+    SellHistory []SellHistory `json:"sellHistory,omitempty"`
+    ProductImages interface{} `json:"productImages,omitempty"`
+}
+
+type SellHistory struct {
+    BuyerEmail, BuyerName, BuyerId string
+    Buantity  int64
+}
+
+type UpdateProductModel struct {
+    Id string `json:"id,omitempty" validate:"required"`
+    Key string `json:"key,omitempty" validate:"required"`
+    Value string `json:"value,omitempty" validate:"required"`
 }
