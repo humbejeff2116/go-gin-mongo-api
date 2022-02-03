99c189d36954016fb5a569bacadd403af44fcc25
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-03 15:22:32 +0000

    implement set cors middleware function

---
+++ configs/setCors.go
@@ -0,0 +1,27 @@
+
+
+
+
+package configs
+
+import (
+	"time"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func SetCors() gin.HandlerFunc{
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://loaclhost:3000"},
+		AllowMethods:     []string{"POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+		  return origin == "http://loaclhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	})
+}
