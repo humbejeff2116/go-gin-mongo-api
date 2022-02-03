8ba4fe4d0368b826a2bf8c999fb9789cee5816e2
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-03 15:44:25 +0000

    modify and clean server code

--- server.go
+++ server.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	// "fmt"
-	// "net/http"
 	"go-gin-mongo-api/configs"
-	"github.com/gin-gonic/gin"
 	"go-gin-mongo-api/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 
 func main() {
-	router := gin.Default();
+	app := gin.Default();
 	serverConfigs := configs.SetServerConfigurations();
 	// connect to database
 	configs.ConnectToMongDb();
-
-	//use products router
-    routes.ProductRoutes(router)
-
+	// disable trust all proxies for now as no proxy client has been used to make request to server
+	app.SetTrustedProxies(nil)
+	// enable cors
+	app.Use(configs.SetCors())
+	// use logger middleware
+	app.Use(gin.Logger())
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	app.Use(gin.Recovery())
+	// api/v1 routes group
+	apiVersionOne := app.Group("/api/v1")
+	// products routes
+    routes.ProductRoutes(apiVersionOne)
+	// users routes
+	routes.UsersRoutes(apiVersionOne)
 	// start  the API server
-	router.Run(serverConfigs.Port);
-	
+	app.Run(serverConfigs.Port)	
 }
