b629f34ae4309d62397695e5aa4572670483d1ce
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-25 20:19:21 +0100

    fix MongoDB double connection bug and clean main package code

--- server.go
+++ server.go
@@ -7,26 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
 	app := gin.Default();
-	serverConfigs := configs.SetServerConfigurations();
-	// connect to database
-	configs.ConnectToMongDb();
-	// disable trust all proxies for now as no proxy client has been used to make request to server
+	// disable trust all proxies 
 	app.SetTrustedProxies(nil)
-	// enable cors
 	app.Use(configs.SetCors())
-	// use logger middleware
-	app.Use(gin.Logger())
-	// Recovery middleware recovers from any panics and writes a 500 if there was one.
-	app.Use(gin.Recovery())
-	// api/v1 routes group
 	apiVersionOne := app.Group("/api/v1")
-	// products routes
     routes.ProductRoutes(apiVersionOne)
-	// users routes
-	routes.UsersRoutes(apiVersionOne)
-	// start  the API server
-	app.Run(serverConfigs.Port)	
+	app.Run(configs.ServerConfigurations.Port)	
 }
